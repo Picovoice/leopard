610e490c8cb71f8b6e039d0bf362cbc334fab081
Alireza Kenarsari <alireza@picovoice.ai>
2022-01-06 18:07:09 -0800

    wip

--- demo/c/leopard_demo.c
+++ demo/c/leopard_demo.c
@@ -1,5 +1,5 @@
 /*
-    Copyright 2019 Picovoice Inc.
+    Copyright 2019-2022 Picovoice Inc.
 
     You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
     file accompanying this source.
@@ -9,24 +9,107 @@
     specific language governing permissions and limitations under the License.
 */
 
-#include <dlfcn.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+
+#if defined(_WIN32) || defined(_WIN64)
+
+#include <windows.h>
+
+#else
+
+#include <dlfcn.h>
+
+#endif
 
 #include "pv_leopard.h"
 
+static void *open_dl(const char *dl_path) {
+
+#if defined(_WIN32) || defined(_WIN64)
+
+    return LoadLibrary(dl_path);
+
+#else
+
+    return dlopen(dl_path, RTLD_NOW);
+
+#endif
+
+}
+
+static void *load_symbol(void *handle, const char *symbol) {
+
+#if defined(_WIN32) || defined(_WIN64)
+
+    return GetProcAddress((HMODULE) handle, symbol);
+
+#else
+
+    return dlsym(handle, symbol);
+
+#endif
+
+}
+
+static void close_dl(void *handle) {
+
+#if defined(_WIN32) || defined(_WIN64)
+
+    FreeLibrary((HMODULE) handle);
+
+#else
+
+    dlclose(handle);
+
+#endif
+
+}
+
+static void print_dl_error(const char *message) {
+
+#if defined(_WIN32) || defined(_WIN64)
+
+    fprintf(stderr, "%s with code '%lu'.\n", message, GetLastError());
+
+#else
+
+    fprintf(stderr, "%s with '%s'.\n", message, dlerror());
+
+#endif
+
+}
+
 int main(int argc, char **argv) {
-    if (argc < 6) {
-        fprintf(
-                stderr,
-                "usage: %s dynamic_library_path acoustic_model_path language_model_path audio_file_path\n",
-                argv[0]);
+    const char *access_key = NULL;
+    const char *library_path = NULL;
+    const char *model_path = NULL;
+
+    int opt = -1;
+    while ((opt = getopt(argc, argv, "a:l:m:")) != -1) {
+        switch (opt) {
+            case 'a':
+                access_key = optarg;
+                break;
+            case 'l':
+                library_path = optarg;
+                break;
+            case 'm':
+                model_path = optarg;
+                break;
+            default:
+                break;
+        }
+    }
+
+    if (!(access_key && library_path && model_path && (argc > 4))) {
+        fprintf(stderr, "-a ACCESS_KEY -l LIBRARY_PATH -m MODEL_PATH wav_path0 wav_path1 ...\n");
         exit(1);
     }
 
-    const char *library_path = argv[1];
-
-    void *dl_handle = dlopen(library_path, RTLD_NOW);
+    void *dl_handle = open_dl(library_path);
     if (!dl_handle) {
         fprintf(stderr, "failed to load dynamic library at '%s'.\n", library_path);
         exit(1);
@@ -40,7 +123,7 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    pv_status_t (*pv_leopard_init)(const char *, const char *, const char *, pv_leopard_t **) =
+    pv_status_t (*pv_leopard_init)(const char *, const char *, pv_leopard_t **) =
     dlsym(dl_handle, "pv_leopard_init");
     if ((error = dlerror()) != NULL) {
         fprintf(stderr, "failed to load 'pv_leopard_init' with '%s'.\n", error);
@@ -60,52 +143,49 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    const char *acoustic_model_path = argv[2];
-    const char *language_model_path = argv[3];
-    const char *license_path = argv[4];
-
     pv_leopard_t *leopard;
-    pv_status_t status = pv_leopard_init(acoustic_model_path, language_model_path, license_path, &leopard);
+    pv_status_t status = pv_leopard_init(access_key, model_path, &leopard);
     if (status != PV_STATUS_SUCCESS) {
         fprintf(stderr, "failed to init with '%s'.\n", pv_status_to_string(status));
         exit(1);
     }
 
-    const char *wav_path = argv[5];
-    FILE *wav_handle = fopen(wav_path, "rb");
-    if (!wav_handle) {
-        fprintf(stderr, "failed to open wav file located at '%s'.\n", wav_path);
-        exit(1);
+    for (int32_t i = 0; i < (argc - optind); i++) {
+        FILE *wav_handle = fopen(argv[i + optind], "rb");
+        if (!wav_handle) {
+            fprintf(stderr, "failed to open wav file located at '%s'.\n", argv[i + optind]);
+            exit(1);
+        }
+
+        static const int32_t WAV_HEADER_LENGTH_BYTE = 44;
+
+        fseek(wav_handle, 0, SEEK_END);
+        const int32_t pcm_length_byte = ftell(wav_handle) - WAV_HEADER_LENGTH_BYTE;
+        const int32_t num_samples = pcm_length_byte / sizeof(int16_t);
+        fseek(wav_handle, WAV_HEADER_LENGTH_BYTE, SEEK_SET);
+
+        int16_t *pcm = malloc(pcm_length_byte);
+        if (!pcm) {
+            fprintf(stderr, "failed to allocate memory for audio buffer\n");
+            exit(1);
+        }
+
+        const size_t count = fread(pcm, sizeof(int16_t), num_samples, wav_handle);
+        if (count != (size_t) num_samples) {
+            fprintf(stderr, "failed to read audio data from '%s'", argv[i + optind]);
+            exit(1);
+        }
+
+        char *transcript;
+        status = pv_leopard_process(leopard, pcm, num_samples, &transcript);
+        if (status != PV_STATUS_SUCCESS) {
+            fprintf(stderr, "failed to process with '%s'.\n", pv_status_to_string(status));
+            exit(1);
+        }
+
+        fprintf(stdout, "%s\n", transcript);
+        fflush(stdout);
     }
-
-    static const int32_t WAV_HEADER_LENGTH_BYTE = 44;
-
-    fseek(wav_handle, 0, SEEK_END);
-    const int32_t pcm_length_byte = ftell(wav_handle) - WAV_HEADER_LENGTH_BYTE;
-    const int32_t num_samples = pcm_length_byte / sizeof(int16_t);
-    fseek(wav_handle, WAV_HEADER_LENGTH_BYTE, SEEK_SET);
-
-    int16_t *pcm = malloc(pcm_length_byte);
-    if (!pcm) {
-        fprintf(stderr, "failed to allocate memory for audio buffer\n");
-        exit(1);
-    }
-
-    const size_t count = fread(pcm, sizeof(int16_t), num_samples, wav_handle);
-    if (count != (size_t) num_samples) {
-        fprintf(stderr, "failed to read audio data from '%s'", wav_path);
-        exit(1);
-    }
-
-    char *transcript;
-    status = pv_leopard_process(leopard, pcm, num_samples, &transcript);
-    if (status != PV_STATUS_SUCCESS) {
-        fprintf(stderr, "failed to process with '%s'.\n", pv_status_to_string(status));
-        exit(1);
-    }
-
-    fprintf(stdout, "%s\n", transcript);
-    fflush(stdout);
 
     pv_leopard_delete(leopard);
 
