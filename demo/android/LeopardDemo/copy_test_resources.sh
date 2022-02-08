23e3a04278d16fb64c5d1824471a0ec508dc6bbd
Eric Mikulin <ericm99@gmail.com>
2022-02-07 17:13:14 -0800

    Add leopard model as test resource

--- demo/android/LeopardDemo/copy_test_resources.sh
+++ demo/android/LeopardDemo/copy_test_resources.sh
@@ -6,3 +6,6 @@ fi
 
 echo "Copying test audio samples..."
 cp ../../../resources/audio_samples/test.wav ./leopard-demo-app/src/androidTest/assets/test_resources/audio/test.wav
+
+echo "Copying leopard model..."
+cp ./leopard-demo-app/src/main/assets/leopard_params.pv ./leopard-demo-app/src/androidTest/assets/test_resources/leopard_params.pv
--- demo/android/LeopardDemo/leopard-demo-app/src/androidTest/java/ai/picovoice/leoparddemo/LeopardTest.java
+++ demo/android/LeopardDemo/leopard-demo-app/src/androidTest/java/ai/picovoice/leoparddemo/LeopardTest.java
@@ -3,7 +3,7 @@
 
     You may not use this file except in compliance with the license. A copy of the license is
     located in the "LICENSE" file accompanying this source.
-    
+
     Unless required by applicable law or agreed to in writing, software distributed under the
     License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
     express or implied. See the License for the specific language governing permissions and
@@ -71,7 +71,7 @@ public class LeopardTest {
         assetManager = testContext.getAssets();
         extractAssetsRecursively("test_resources");
         testResourcesPath = new File(appContext.getFilesDir(), "test_resources").getAbsolutePath();
-        defaultModelPath = new File(appContext.getFilesDir(), "leopard_params.pv").getAbsolutePath();
+        defaultModelPath = new File(testResourcesPath, "leopard_params.pv").getAbsolutePath();
 
         accessKey = appContext.getString(R.string.pvTestingAccessKey);
     }
