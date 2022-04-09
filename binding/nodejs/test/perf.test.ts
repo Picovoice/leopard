3c888611df581f642f2db17e3a67143dc4e092e8
Ian Lavery <ian@picovoice.ai>
2022-04-08 17:50:55 -0700

    Node performance tests (#57)

---
+++ binding/nodejs/test/perf.test.ts
@@ -0,0 +1,83 @@
+//
+// Copyright 2022 Picovoice Inc.
+//
+// You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
+// file accompanying this source.
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//
+"use strict";
+
+import { Leopard } from "../src";
+import * as path from "path";
+import { performance } from "perf_hooks";
+
+const WAV_PATH = "../../../resources/audio_samples/test.wav";
+
+const ACCESS_KEY =
+  process.argv
+    .filter((x) => x.startsWith("--access_key="))[0]
+    ?.split("--access_key=")[1] ?? "";
+const NUM_TEST_ITERATIONS = Number(
+  process.argv
+    .filter((x) => x.startsWith("--num_test_iterations="))[0]
+    ?.split("--num_test_iterations=")[1] ?? 0
+);
+const INIT_PERFORMANCE_THRESHOLD_SEC = Number(
+  process.argv
+    .filter((x) => x.startsWith("--init_performance_threshold_sec="))[0]
+    ?.split("--init_performance_threshold_sec=")[1] ?? 0
+);
+const PROC_PERFORMANCE_THRESHOLD_SEC = Number(
+  process.argv
+    .filter((x) => x.startsWith("--proc_performance_threshold_sec="))[0]
+    ?.split("--proc_performance_threshold_sec=")[1] ?? 0
+);
+
+describe("Performance", () => {
+  test("init performance", () => {
+    let perf_results = [];
+    for (let i = 0; i < NUM_TEST_ITERATIONS; i++) {
+      const before = performance.now();
+      let leopardEngine = new Leopard(ACCESS_KEY);
+      let init_time = performance.now() - before;
+
+      leopardEngine.release();
+
+      if (i > 0) {
+        perf_results.push(init_time);
+      }
+    }
+
+    let avgPerfMs =
+      perf_results.reduce((acc, a) => acc + a, 0) / NUM_TEST_ITERATIONS;
+    let avgPerfSec = Number((avgPerfMs / 1000).toFixed(3));
+    console.log("Average init performance: " + avgPerfSec);
+    expect(avgPerfSec).toBeLessThanOrEqual(INIT_PERFORMANCE_THRESHOLD_SEC);
+  });
+
+  test("proc performance", () => {
+    const waveFilePath = path.join(__dirname, WAV_PATH);
+    let leopardEngine = new Leopard(ACCESS_KEY);
+
+    let perf_results = [];
+    for (let i = 0; i < NUM_TEST_ITERATIONS; i++) {
+      const before = performance.now();
+      leopardEngine.processFile(waveFilePath);
+      let proc_time = performance.now() - before;
+
+      if (i > 0) {
+        perf_results.push(proc_time);
+      }
+    }
+    leopardEngine.release();
+
+    let avgPerfMs =
+      perf_results.reduce((acc, a) => acc + a, 0) / NUM_TEST_ITERATIONS;
+    let avgPerfSec = Number((avgPerfMs / 1000).toFixed(3));
+    console.log("Average proc performance: " + avgPerfSec);
+    expect(avgPerfSec).toBeLessThanOrEqual(PROC_PERFORMANCE_THRESHOLD_SEC);
+  });
+});
