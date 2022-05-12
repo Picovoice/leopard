e3b2bcb53f8bf497ee854ae78f45234a65b5a95a
Reza (Pouya) Rostam <42750891+mrrostam@users.noreply.github.com>
2022-05-12 15:29:33 -0700

    Python release (#73)

--- binding/python/setup.py
+++ binding/python/setup.py
@@ -49,7 +49,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
 
 setuptools.setup(
     name="pvleopard",
-    version="1.0.5",
+    version="1.0.6",
     author="Picovoice",
     author_email="hello@picovoice.ai",
     description="Leopard Speech-to-Text Engine.",
