4312a9d999530a7b3d9d25e2cb009496ccb1d3a4
Kwangsoo Yeo <kyeo@picovoice.ai>
2022-05-13 11:32:25 -0700

    v1.0 flutter patch (#80)

--- binding/flutter/lib/leopard.dart
+++ binding/flutter/lib/leopard.dart
@@ -11,10 +11,8 @@
 
 import 'dart:async';
 import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:path_provider/path_provider.dart';
 import 'package:leopard_flutter/leopard_error.dart';
 
