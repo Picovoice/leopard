ce280b9d038672f450fd34eac09eb1deb2688517
Alireza Kenarsari <alireza@picovoice.ai>
2022-08-21 16:05:40 -0700

    wip

--- demo/python-subtitle/main.py
+++ demo/python-subtitle/main.py
@@ -55,8 +55,8 @@ def main() -> None:
     parser = ArgumentParser()
     parser.add_argument('--access-key', required=True)
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--youtube-url')
-    group.add_argument('--audio-path')
+    group.add_argument('--youtube-url', default=None)
+    group.add_argument('--audio-path', default=None)
     parser.add_argument('--subtitle-path', required=True)
     parser.add_argument('--model-path', default=None)
     args = parser.parse_args()
@@ -71,6 +71,41 @@ def main() -> None:
     anime.start()
     leopard = pvleopard.create(access_key=access_key, model_path=model_path)
     anime.stop()
+
+    if audio_path is not None:
+        if not os.path.exists(audio_path):
+            raise ValueError(f"`{audio_path}` does not exist")
+    else:
+        audio_path = os.path.join(os.path.dirname(subtitle_path), '%s.webm' % youtube_url.split("watch?v=")[1])
+
+    if youtube_url is not None and not os.path.exists(audio_path):
+        anime = ProgressAnimation('Downloading `%s`' % youtube_url)
+        anime.start()
+        youtube = YouTube(youtube_url)
+        audio_stream = youtube.streams.filter(mime_type='audio/webm').order_by('bitrate').last()
+        audio_stream.download(
+            output_path=os.path.dirname(audio_path),
+            filename=os.path.basename(audio_path),
+            skip_existing=True)
+        anime.stop()
+
+    try:
+        anime = ProgressAnimation('Transcribing `%s`' % youtube_url)
+        anime.start()
+        start_sec = time.time()
+        transcript, words = leopard.process_file(audio_path)
+        proc_sec = time.time() - start_sec
+        anime.stop()
+        print("Transcribed `%.2f` seconds" % proc_sec)
+
+        if os.path.exists(subtitle_path):
+            os.remove(subtitle_path)
+        with open(subtitle_path, 'w') as f:
+            f.write(transcript)
+            f.write('\n')
+        print('Saved transcription into `%s`' % subtitle_path)
+    finally:
+        pass
 
 
 if __name__ == '__main__':
