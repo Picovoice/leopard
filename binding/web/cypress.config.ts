94621100f963a98fb13075046b9cd2f72e1f7e5f
Kwangsoo Yeo <kyeo@picovoice.ai>
2023-03-21 16:15:23 -0700

    fix build

--- binding/web/cypress.config.ts
+++ binding/web/cypress.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     "PROC_PERFORMANCE_THRESHOLD_SEC": 0.8
   },
   e2e: {
-    defaultCommandTimeout: 60000,
+    defaultCommandTimeout: 90000,
     supportFile: "cypress/support/index.ts",
     specPattern: "test/*.test.{js,jsx,ts,tsx}",
     video: false,
