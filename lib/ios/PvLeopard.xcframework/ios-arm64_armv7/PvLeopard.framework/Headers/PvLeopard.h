5423ce851fa28743e50b854a1a5dbb91715d3b1b
Ian <ian@picovoice.ai>
2023-03-21 12:30:12 -0700

    libs

--- lib/ios/PvLeopard.xcframework/ios-arm64_armv7/PvLeopard.framework/Headers/PvLeopard.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for PvLeopard.
-FOUNDATION_EXPORT double PvLeopardVersionNumber;
-
-//! Project version string for PvLeopard.
-FOUNDATION_EXPORT const unsigned char PvLeopardVersionString[];
