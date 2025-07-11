cb0e742a4011b9894e7fa7f05be5d095d8892fe0
pico-mo <mo@picovoice.ai>
2025-07-11 12:00:15 -0700

    import formatting and whitespace
    Co-authored-by: Kwangsoo Yeo <kyeo@picovoice.ai>

--- binding/java/test/ai/picovoice/leopard/LeopardTest.java
+++ binding/java/test/ai/picovoice/leopard/LeopardTest.java
@@ -33,9 +33,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.stream.Stream;
-
 import java.util.Locale;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
