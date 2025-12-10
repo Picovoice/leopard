9e6ef4c05e0baf2fed52a554448bd95cb24cc510
Kwangsoo Yeo <kyeo@picovoice.ai>
2025-12-10 14:23:07 -0800

    fix code style

--- binding/android/Leopard/leopard/src/main/java/ai/picovoice/leopard/Leopard.java
+++ binding/android/Leopard/leopard/src/main/java/ai/picovoice/leopard/Leopard.java
@@ -65,11 +65,13 @@ public class Leopard {
      *
      * @param accessKey                  AccessKey obtained from Picovoice Console
      * @param modelPath                  Absolute path to the file containing Leopard model parameters.
-     * @param device String representation of the device (e.g., CPU or GPU) to use. If set to `best`, the most
-     * suitable device is selected automatically. If set to `gpu`, the engine uses the first available GPU device. To select a specific
-     * GPU device, set this argument to `gpu:${GPU_INDEX}`, where `${GPU_INDEX}` is the index of the target GPU. If set to
-     * `cpu`, the engine will run on the CPU with the default number of threads. To specify the number of threads, set this
-     * argument to `cpu:${NUM_THREADS}`, where `${NUM_THREADS}` is the desired number of threads.
+     * @param device String representation of the device (e.g., CPU or GPU) to use for inference.
+     *               If set to `best`, the most suitable device is selected automatically. If set to `gpu`,
+     *               the engine uses the first available GPU device. To select a specific GPU device, set this
+     *               argument to `gpu:${GPU_INDEX}`, where `${GPU_INDEX}` is the index of the target GPU. If
+     *               set to `cpu`, the engine will run on the CPU with the default number of threads. To specify
+     *               the number of threads, set this argument to `cpu:${NUM_THREADS}`, where `${NUM_THREADS}`
+     *               is the desired number of threads.
      * @param enableAutomaticPunctuation Set to `true` to enable automatic punctuation insertion.
      * @param enableDiarization          Set to `true` to enable speaker diarization, which allows Leopard to
      *                                   differentiate speakers as part of the transcription process. Word
